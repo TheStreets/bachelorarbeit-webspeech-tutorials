898975d6f9886a19cd3e9fa6a26410e717857c17
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-07-08 00:53:30 +0200

    added homepage with styles

--- webspeech-tutorials/src/app/components/homepage/homepage.component.ts
+++ webspeech-tutorials/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  hciLogo: string = '../../assets/hci_logo.svg';
+  luhLogo: string = '../../assets/luh_logo.svg';
+  illustration: string = '../../assets/illustration.svg';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
