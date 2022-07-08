57244e7544c00a1a8890b9b915374adc22575d27
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-07-08 20:32:55 +0200

    created component

---
+++ webspeech-tutorials/src/app/components/name-the-bot/name-the-bot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-the-bot',
+  templateUrl: './name-the-bot.component.html',
+  styleUrls: ['./name-the-bot.component.css']
+})
+export class NameTheBotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
