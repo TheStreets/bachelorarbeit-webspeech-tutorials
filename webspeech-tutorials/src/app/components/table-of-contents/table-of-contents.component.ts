edcedc215566672a233c38c5f8f52eab600fad94
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-07-13 16:43:07 +0200

    component TableOfContentsComponent

---
+++ webspeech-tutorials/src/app/components/table-of-contents/table-of-contents.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {faCircle} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-table-of-contents',
+  templateUrl: './table-of-contents.component.html',
+  styleUrls: ['./table-of-contents.component.css']
+})
+export class TableOfContentsComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
