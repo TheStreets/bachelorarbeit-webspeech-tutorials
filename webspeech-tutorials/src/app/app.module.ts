8e31aa6c266dcc6081d0acaa8fde012da6a2382b
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-07-31 19:44:44 +0200

    formatted file; added FontAwesomeModule; added TutorialComponent

--- webspeech-tutorials/src/app/app.module.ts
+++ webspeech-tutorials/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -6,13 +6,15 @@ import {HomepageComponent} from './components/homepage/homepage.component';
 import {NavbarComponent} from './components/side-components/navbar/navbar.component';
 import {NameTheBotComponent} from './components/name-the-bot/name-the-bot.component';
 import {FormsModule} from "@angular/forms";
-import { CtaButtonComponent } from './components/side-components/cta-button/cta-button.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { TableOfContentsComponent } from './components/table-of-contents/table-of-contents.component';
-import { TutorialCardComponent } from './components/side-components/tutorial-card/tutorial-card.component';
+import {CtaButtonComponent} from './components/side-components/cta-button/cta-button.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {TableOfContentsComponent} from './components/table-of-contents/table-of-contents.component';
+import {TutorialCardComponent} from './components/side-components/tutorial-card/tutorial-card.component';
 import {HttpClientModule} from "@angular/common/http";
 import {DataService} from "./services/data.service";
+import {TutorialComponent} from './components/tutorial/tutorial.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -23,16 +25,19 @@ import {DataService} from "./services/data.service";
     CtaButtonComponent,
     PageNotFoundComponent,
     TableOfContentsComponent,
-    TutorialCardComponent
+    TutorialCardComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [DataService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
 }
