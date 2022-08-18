c0031eaec4cec0e0a5951d478ba7d453a0c77fd6
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-08-18 19:13:40 +0200

    handling of the editor data

--- webspeech-tutorials/src/app/components/tutorial/tutorial.component.ts
+++ webspeech-tutorials/src/app/components/tutorial/tutorial.component.ts
@@ -1,17 +1,10 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {createHtml} from "../../utils";
 import * as ace from "ace-builds";
 import 'ace-builds/src-noconflict/ext-language_tools';
 import {Ace} from "ace-builds";
-import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
-import {faWindowClose} from "@fortawesome/free-solid-svg-icons/";
+import {UtilsService} from "../../services/utils.service";
+import {EditorData} from "../../models/editor.data.model";
 
-function speakText(text: string) {
-  const speaker = window.speechSynthesis;
-  const utterance = new SpeechSynthesisUtterance();
-  utterance.text = text;
-  speaker.speak(utterance);
-}
 
 @Component({
   selector: 'app-tutorial',
@@ -29,18 +22,30 @@ export class TutorialComponent implements OnInit, AfterViewInit {
   @ViewChild("htmlEditor")
   private htmlEditor!: ElementRef<HTMLElement>;
 
+  sizeTopLeft: number = 50;
+  sizeTopRight: number = 50;
+  sizes = {
+    percent: {
+      area_1: 50,
+      area_2: 50
+    }
+  }
+  gutterSizeVertical: number = 10;
+
   editorOptions = {
-    'enableBasicAutocompletion': true,
-    'enableLiveAutocompletion': true,
-    'enableSnippets': true
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    enableSnippets: true,
+    autoScrollEditorIntoView: true,
+    copyWithEmptySelection: true,
+
   };
 
-  editorTheme: string = 'ace/theme/twilight';
+  editorTheme: string = 'ace/theme/cobalt';
   jsAceEditor!: Ace.Editor;
   htmlAceEditor!: Ace.Editor;
 
-
-  constructor() {
+  constructor(private utilsService: UtilsService) {
   }
 
   ngOnInit(): void {
@@ -48,30 +53,38 @@ export class TutorialComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    // this.setupEditor();
-    // this.iframe.nativeElement.srcdoc = createHtml(this.htmlAceEditor.session.getValue(), this.jsAceEditor.session.getValue());
-  }
+    this.utilsService.editorDataSubject?.subscribe( (editorData) => {
+      // setting the iframe content from the editor
+      this.iframe.nativeElement.srcdoc = this.utilsService.createTemplate(editorData.html, editorData.js);
+    });
 
-  speak() {
-    speakText('Hallo');
+    this.setupEditor();
+
+    this.htmlAceEditor.session.setValue(this.utilsService.tutorialData.html);
+    this.jsAceEditor.session.setValue(this.utilsService.tutorialData.js);
+
+    // on change event triggered, if someone types in the editor
+    this.htmlAceEditor.on('change', () => {
+      this.utilsService.editorDataSubject?.next(new EditorData(this.htmlAceEditor.getValue(), this.jsAceEditor.getValue()));
+    });
+    this.jsAceEditor.on('change', () => {
+      this.utilsService.editorDataSubject?.next(new EditorData(this.htmlAceEditor.getValue(), this.jsAceEditor.getValue()));
+    });
   }
 
   private setupEditor(): void {
     ace.config.set("fontSize", "14px");
     ace.config.set("basePath", "https://unpkg.com/ace-builds@1.4.12/src-noconflict");
 
-    this.jsAceEditor = ace.edit(this.jsEditor.nativeElement);
+    this.jsAceEditor = ace.edit(this.jsEditor.nativeElement, {selectionStyle: "text"});
     this.jsAceEditor.setOptions(this.editorOptions);
     this.jsAceEditor.setTheme(this.editorTheme);
     this.jsAceEditor.session.setMode('ace/mode/javascript');
 
-    this.htmlAceEditor = ace.edit(this.htmlEditor.nativeElement)
+    this.htmlAceEditor = ace.edit(this.htmlEditor.nativeElement, {selectionStyle: "text"})
     this.htmlAceEditor.setOptions(this.editorOptions);
     this.htmlAceEditor.setTheme(this.editorTheme);
     this.htmlAceEditor.session.setMode('ace/mode/html');
 
-    this.htmlAceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
   }
-
-
 }
