95848e5392b31e540cf4bf2730e4e37d682f7822
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-04 00:21:26 +0200

    importing PortalModule

--- webspeech-tutorials/src/app/app.module.ts
+++ webspeech-tutorials/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DataService} from "./services/data.service";
 import {TutorialComponent} from './components/tutorial/tutorial.component';
 import {AngularSplitModule} from "angular-split";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {PortalModule} from "@angular/cdk/portal";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     HttpClientModule,
     AngularSplitModule,
-    NgbModule
+    NgbModule,
+    PortalModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent],
