30f0701cec0d16774ef11227337139e2803ffa0b
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-03 20:31:40 +0200

    added new service

---
+++ webspeech-tutorials/src/app/services/window-manager/window-manager.service.ts
@@ -0,0 +1,11 @@
+import {ApplicationRef, ComponentFactoryResolver, Injectable, Injector, OnDestroy} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WindowManagerService {
+
+  constructor() {
+  }
+
+}
